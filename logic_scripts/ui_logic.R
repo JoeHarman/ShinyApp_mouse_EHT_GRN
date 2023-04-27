e3b03abe235b484ff2ca05cbbae621a870e1a563
proteinsoup <joerharman@gmail.com>
2023-04-27 23:20:37 +0100

    Layout rearrange

--- logic_scripts/ui_logic.R
+++ logic_scripts/ui_logic.R
@@ -22,73 +22,90 @@ ui <- fluidPage(
       ### RNA panel code ###
       tabPanel("RNA",
 
+        # Annotations row
+        wellPanel(fluidRow(
+          column(4,
+            selectInput("gene", "Choose a gene for expression:",
+              choices = unique(deg))
+          ),
+          column(4,
+            radioButtons(
+              "RNA_exprs_anno", "Colour annotation:", inline = TRUE,
+              choices = c(
+                "Population" = 1,
+                "Stage" = 2,
+                "Sample" = 3)
+            )
+          ),
+          column(4,
+            sliderInput("RNA_topn",
+              "Top variable genes for PCA calculation:",
+              min = 0, max = nrow(RNA_exprs_wide), value = nrow(RNA_exprs_wide))
+          ),
+        )),
+
+        # Plots row
+        fluidRow(
+          column(6, h3("Expression plot"),
+            plotOutput("RNA_exprs") %>%
+              withSpinner(type = 5, color = "#0dc5c1", hide.ui = FALSE)),
+
+          column(6, h3("PCA plot"),
+            plotOutput("RNA_pca") %>%
+              withSpinner(type = 5, color = "#0dc5c1", hide.ui = FALSE))
+        ),
+
         # Stats row
         fluidRow(
           column(8, h3("Differential expression analysis:")),
           DT::dataTableOutput("RNA_stats_tbl", width = "90%") %>%
             withSpinner(type = 5, color = "#0dc5c1")
-        ),
-
-        # Plots row
-        fluidRow(
-          column(6, h3("PCA plot"),
-            plotOutput("RNA_pca") %>%
-              withSpinner(type = 5, color = "#0dc5c1", hide.ui = FALSE)),
-
-          column(6, h3("Expression plot"),
-            selectInput("gene", "Choose a gene for expression:",
-            choices = unique(deg)),
-            plotOutput("RNA_exprs") %>%
-              withSpinner(type = 5, color = "#0dc5c1", hide.ui = FALSE))
-        ),
-
-        # Annotations row
-        fluidRow(
-          sliderInput("RNA_topn", "Top variable genes for PCA calculation:",
-            min = 0, max = nrow(RNA_exprs_wide), value = 5000),
-          radioButtons(
-            "RNA_exprs_anno", "Colour annotation:", inline = TRUE,
-            choices = c(
-              "Population" = 1,
-              "Stage" = 2,
-              "Sample" = 3)
-          ),
         )
+
       ),
 
       ### ATAC panel code ###
       tabPanel("ATAC",
+
+        # Annotations row
+        wellPanel(fluidRow(
+          column(4,
+            selectizeInput("enhancer",
+              "Choose an enhancer for accessibility plot:",
+              choices = NULL)
+          ),
+          column(4,
+            radioButtons(
+              "ATAC_exprs_anno", "Colour annotation:", inline = TRUE,
+              choices = c(
+                "Population" = 1,
+                "Stage" = 2,
+                "Sample" = 3)
+            )
+          ),
+          column(4,
+            sliderInput("ATAC_topn",
+              "Top variable enhancers for PCA calculation:",
+              min = 0, max = nrow(ATAC_exprs_wide), value = 5000)
+          ),
+        )),
+
+        # Plots row
+        fluidRow(
+          column(6, h3("Accessibility plot"),
+            plotOutput("ATAC_exprs") %>%
+              withSpinner(type = 5, color = "#0dc5c1", hide.ui = FALSE)),
+
+          column(6, h3("PCA plot"),
+            plotOutput("ATAC_pca") %>%
+              withSpinner(type = 5, color = "#0dc5c1", hide.ui = FALSE))
+        ),
+
         # Stats row
         fluidRow(
           column(8, h3("Differential accessibility analysis:")),
           DT::dataTableOutput("ATAC_stats_tbl", width = "90%") %>%
             withSpinner(type = 5, color = "#0dc5c1")
-        ),
-
-        # Plots row
-        fluidRow(
-          column(6, h3("PCA plot"),
-            plotOutput("ATAC_pca") %>%
-              withSpinner(type = 5, color = "#0dc5c1", hide.ui = FALSE)),
-          column(6, h3("Accessibility plot"),
-            selectizeInput("enhancer",
-              "Choose an enhancer for accessibility plot:",
-              choices = NULL),
-            plotOutput("ATAC_exprs") %>%
-              withSpinner(type = 5, color = "#0dc5c1", hide.ui = FALSE))
-        ),
-
-        # Annotations row
-        fluidRow(
-          sliderInput("ATAC_topn", "Number of observations:",
-            min = 0, max = nrow(ATAC_exprs_wide), value = 1000),
-          radioButtons(
-            "ATAC_exprs_anno", "Colour annotation:", inline = TRUE,
-            choices = c(
-              "Population" = 1,
-              "Stage" = 2,
-              "Sample" = 3)
-          )
         )
       )
     ), width = 10)
